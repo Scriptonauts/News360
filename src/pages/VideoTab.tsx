fb61fbd16ddf43f40f6f720f3bfc7e19f53de296
Kweku Bright <kweku01bright@gmail.com>
2022-07-06 02:19:06 +0100

    Refactor app header into component

--- src/pages/VideoTab.tsx
+++ src/pages/VideoTab.tsx
@@ -1,29 +1,15 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonPage} from '@ionic/react';
 import React from 'react';
+import AppHeader from '../components/AppHeader';
 import VideoList from '../components/VideoList';
 
 const VideoTab: React.FC = () => {
 
   return (
     <IonPage>
-      <IonHeader style={{ backgroundColor: '#ad0000', color: '#ffffff' }}>
-        <IonToolbar color='#33cc66'>
-          <IonButtons slot="start">
-            <IonBackButton defaultHref="/home" />
-          </IonButtons>
-          <IonTitle>Shows </IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <AppHeader title="Videos" backButton={ true } />
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Videos</IonTitle>
-
-          </IonToolbar>
-        </IonHeader>
-
         <VideoList></VideoList>
-
       </IonContent>
     </IonPage>
   );
